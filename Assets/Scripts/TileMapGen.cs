0f32f73230390e6b269f3cc067a916767cae7afa
theclone <mrperson7895@gmail.com>
2020-12-02 09:36:15 -0800

    Add deterministic generation functions

--- Assets/Scripts/TileMapGen.cs
+++ Assets/Scripts/TileMapGen.cs
@@ -5,7 +5,14 @@ using UnityEngine.Tilemaps;
 
 public class TileMapGen : MonoBehaviour
 {
+    [System.Serializable]
+    public struct MapGenOption {
+        TileBase tileBase;
+        int minDistBetweenTiles;
+    }
     //public static TileMapGen Instance{set;get;}
+    public MapGenOption[] mapGenOptions;
+    public int testSeed;
     public TileBase ground1, ground2, ground3;
     public BoundsInt tileArea;
     private Tilemap tileMap;
@@ -13,7 +20,17 @@ public class TileMapGen : MonoBehaviour
 
     public void Start()
     {
-        
+        GenerateWithSeed(testSeed);
+    }
+
+    void Update()
+    {
+        #if UNITY_EDITOR
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GenerateWithSeed(testSeed);
+            }
+        #endif
     }
     
     public void Generate()
@@ -21,6 +38,37 @@ public class TileMapGen : MonoBehaviour
         intMap = new int[tileArea.size.x, tileArea.size.y];
         PoissonPlace(1, 5, ref intMap, 30);
         PoissonPlace(2, 5, ref intMap, 30);
+        tileMap = GetComponent<Tilemap>();
+        TileBase[] tileArray = new TileBase[tileArea.size.x * tileArea.size.y * tileArea.size.z];
+        for (int y = 0; y < tileArea.size.y; y++)
+        {
+            for (int x = 0; x < tileArea.size.x; x++)
+            {
+                switch (intMap[x, y])
+                {
+                    case 2:
+                        tileArray[y * tileArea.size.x + x] = ground3;
+                        break; 
+                    case 1:
+                        tileArray[y * tileArea.size.x + x] = ground2;
+                        break;                  
+                    default:
+                        tileArray[y * tileArea.size.x + x] = ground1;
+                        break;
+                }
+            }
+        }
+
+        tileMap.SetTilesBlock(tileArea, tileArray);
+    }
+
+    public void GenerateWithSeed(int seed)
+    {
+        System.Random generator = new System.Random(seed);
+
+        intMap = new int[tileArea.size.x, tileArea.size.y];
+        PoissonPlaceSeed(1, 5, ref generator, ref intMap, 30);
+        PoissonPlaceSeed(2, 5, ref generator, ref intMap, 30);
         tileMap = GetComponent<Tilemap>();
         TileBase[] tileArray = new TileBase[tileArea.size.x * tileArea.size.y * tileArea.size.z];
         for (int y = 0; y < tileArea.size.y; y++)
@@ -76,6 +124,68 @@ public class TileMapGen : MonoBehaviour
             {
                 Vector2Int potentialSample = chosenSample;
                 Vector2 randomDir = Random.insideUnitCircle;
+                randomDir = randomDir.normalized * radius + randomDir * radius;
+
+                potentialSample.x += Mathf.RoundToInt(randomDir.x);
+                potentialSample.y += Mathf.RoundToInt(randomDir.y);
+
+                if (potentialSample.x < 0 || potentialSample.y < 0 || potentialSample.x >= tileMap.GetLength(0) || potentialSample.y >= tileMap.GetLength(1))
+                {
+                    continue;
+                }
+
+                //Debug.Log("Checking " + potentialSample);
+
+                if (poissonGridCheck(potentialSample, cellSize, radius, ref bg_grid, ref points))
+                {
+                    foundCandidate = true;
+                    activeSample.Enqueue(potentialSample);
+                    bg_grid[Mathf.FloorToInt(initialPoint.x / cellSize), Mathf.FloorToInt(initialPoint.y / cellSize)] = points.Count;
+                    points.Add(potentialSample);
+                    tileMap[potentialSample.x, potentialSample.y] = tileType;
+                }
+            }
+
+            if (!foundCandidate)
+            {
+                activeSample.Dequeue();
+            }
+        }
+        return points;
+    }
+
+    public List<Vector2Int> PoissonPlaceSeed(int tileType, int radius, ref System.Random rand, ref int[,] tileMap, int rejectionLimit = 30)
+    {
+        List<Vector2Int> points = new List<Vector2Int>();
+
+        Queue<Vector2Int> activeSample = new Queue<Vector2Int>();
+
+        float cellSize = radius / 1.4f;
+        int[,] bg_grid = new int[Mathf.CeilToInt(tileMap.GetLength(0) / cellSize), Mathf.CeilToInt(tileMap.GetLength(1) / cellSize)];
+        for (int i = 0; i < bg_grid.GetLength(0); i++) 
+        {
+            for (int j = 0; j < bg_grid.GetLength(1); j++) 
+            {
+                bg_grid[i, j] = -1;
+            }
+        }       
+
+        Vector2Int initialPoint = new Vector2Int(rand.Next(0, tileMap.GetLength(0)), rand.Next(0, tileMap.GetLength(1)));
+        activeSample.Enqueue(initialPoint);
+        points.Add(initialPoint);
+        tileMap[initialPoint.x, initialPoint.y] = tileType;
+        bg_grid[Mathf.FloorToInt(initialPoint.x / cellSize), Mathf.FloorToInt(initialPoint.y / cellSize)] = 0;
+
+        while (activeSample.Count > 0)
+        {
+            Vector2Int chosenSample = activeSample.Peek();
+            bool foundCandidate = false;
+
+            for (int i = 0; i < rejectionLimit; i++)
+            {
+                Vector2Int potentialSample = chosenSample;
+                float randRadians = (float)rand.NextDouble() * Mathf.Deg2Rad;
+                Vector2 randomDir = new Vector2(Mathf.Cos(radius), Mathf.Sin(radius)) * (float)rand.NextDouble();
                 randomDir = randomDir.normalized * radius + randomDir * radius;
 
                 potentialSample.x += Mathf.RoundToInt(randomDir.x);
